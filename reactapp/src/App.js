d91ebf3ab99cc55037d0adb53150b2aa8a4c6235
Iamneo <techadmin@iamneo.ai>
2023-09-27 10:36:02 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,46 +1,25 @@
-import { fireEvent, getByLabelText, render, screen } from '@testing-library/react';
-import App from '../App';
+import logo from './logo.svg';
+import './App.css';
 
-test('renders_Greeting_word', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Greeting/i);
-  expect(linkElement).toBeInTheDocument();
-});
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
-test('render_label_For_entry', () => {
-  render(<App/>);
-  const labelid = screen.getByTestId("label");
-  expect(labelid).toBeInTheDocument();
-})
-
-test('render_form', () => {
-  render(<App/>);
-  const formelement = screen.getByTestId("form");
-  expect(formelement);
-})
-
-test('render_button',() => {
-  render(<App/>);
-  const buttonId = screen.getByRole("button", {name : /Say Hello/i });
-  expect(buttonId).toBeInTheDocument();
-})
-
-test('handlechange_update_name_status', () => {
-  const {getByLabelText} = render(<App/>);
-  const getChangeelement = getByLabelText(/Enter your name/i);
-  fireEvent.change(getChangeelement,{target :{value : 'Ragu'}});
-  expect(getChangeelement.value).toBe('Ragu');
-})
-
-test('handleClick_sets_the_current_name', () => {
-  const {getByLabelText, getByText} = render(<App/>);
-  const getChangeelement = getByLabelText(/Enter your name/i);
-  const submitchange = getByText(/Say Hello/i);
-
-  fireEvent.change(getChangeelement,{target :{value : 'vijay'}});
-  fireEvent.click(submitchange);
-
-  const name = getByText(/Hello, vijay!/i);
-  expect(name).toBeInTheDocument("Hello, vijay!");
-})
-
+export default App;
